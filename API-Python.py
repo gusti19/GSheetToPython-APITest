c93c7490c54020254dd0119e3dec6a569ae8e293
gusti19 <127907756+gusti19@users.noreply.github.com>
2024-03-02 22:12:00 +0700

    Update API-Python.py

--- API-Python.py
+++ API-Python.py
@@ -1,10 +1,10 @@
 import requests
 
 # Tentukan kunci API
-api_key = 'AIzaSyBe4Ux0K0qx0bCheg6gsi4KlZLSsp3HFJY'
+api_key = 'API-mu dewe lah'
 
 # URL untuk mengambil data dari spreadsheet
-url = f"https://sheets.googleapis.com/v4/spreadsheets/1IwJIkJa0GhTnK1DZJFc38vJxlqQNrMrZU3ipffMXCDg/values/Number!A1:B2?key={api_key}"
+url = f"https://sheets.googleapis.com/v4/spreadsheets/BLABLABLA/values/Number!A1:B2?key={api_key}"
 
 # Lakukan permintaan GET untuk mendapatkan data
 response = requests.get(url)
